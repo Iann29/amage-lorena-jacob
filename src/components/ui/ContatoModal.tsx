d3405412c0d59be65b03e89aba4b8285fc20960e
Iann29 <iansingshau@gmail.com>
2025-05-01 18:12:09 -0300

    Ajuste no tamanho dos ícones

--- src/components/ui/ContatoModal.tsx
+++ src/components/ui/ContatoModal.tsx
@@ -81,8 +81,8 @@ export function ContatoModal({ isOpen, onClose }: ContatoModalProps) {
           <Image 
             src="https://vqldbbetnfhzealxumcl.supabase.co/storage/v1/object/public/lorena-images-db/icons/whatsModal.svg"
             alt="Ícone do WhatsApp"
-            width={24}
-            height={24}
+            width={32}
+            height={32}
             className={styles.whatsappIcon}
           />
           Chamar no WhatsApp
@@ -97,15 +97,15 @@ export function ContatoModal({ isOpen, onClose }: ContatoModalProps) {
           <Image 
             src="https://vqldbbetnfhzealxumcl.supabase.co/storage/v1/object/public/lorena-images-db/icons/instaModal.svg"
             alt="Ícone do Instagram"
-            width={24}
-            height={24}
+            width={32}
+            height={32}
             className={styles.instagramIcon}
           />
           Siga-me no Instagram
         </a>
 
         <div className={styles.emailContainer}>
-          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.emailIcon}>
+          <svg width="32" height="32" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.emailIcon}>
             <path d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 8L12 13L4 8V6L12 11L20 6V8Z" fill="#333333"/>
           </svg>
           <span className={styles.emailText}>lorenajacobterapeuta@gmail.com</span>
