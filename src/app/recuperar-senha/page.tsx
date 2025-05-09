60f1245d1e1ac04611b80d71b31b69d20d98da09
Iann29 <iansingshau@gmail.com>
2025-05-09 18:46:21 -0300

    Arrumando problema no build

--- src/app/recuperar-senha/page.tsx
+++ src/app/recuperar-senha/page.tsx
@@ -184,7 +184,7 @@ function RecuperarSenhaContent() {
   }
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.themeVariables} ${styles.container}`}>
       <Link href="/login" className={styles.backButton}>
         <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M15 8H1M1 8L8 15M1 8L8 1" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg>
         Ir para Login
